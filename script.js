820234331b380e80aa2805ab18ade010d2be1691
Ahmed ELsaman <129955390+a7m3delsman@users.noreply.github.com>
2025-02-03 16:10:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,55 +1,39 @@
 function displayVideo() {
-    var videoUrl = document.getElementById('videoInput').value;
-    var videoFrame = document.getElementById('videoFrame');
-    var videoId = extractVideoId(videoUrl);
-    var embedUrl = 'https://www.youtube.com/embed/' + videoId;
+  var videoUrl = document.getElementById('videoInput').value;
+  var videoFrame = document.getElementById('videoFrame');
+  var videoId = extractVideoId(videoUrl);
 
-    videoFrame.setAttribute('src', embedUrl);
+  if (videoId) {
+      var embedUrl = 'https://www.youtube.com/embed/' + videoId;
+      videoFrame.setAttribute('src', embedUrl);
+      showElement(videoFrame);
+      hideElement(document.getElementById('error'));
+  } else {
+      hideElement(videoFrame);
+      showElement(document.getElementById('error'));
+  }
 }
 
 function extractVideoId(url) {
-    var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
-    var match = url.match(regExp);
+ 
+  var regExp = /^.*(youtu\.be\/|v\/|u\/\w\/|embed\/|live\/|watch\?v=|&v=)([^#\&\?]{11}).*/;
+  var match = url.match(regExp);
 
-    if (match && match[7].length === 11) {
-        hideP("error");
-        nohideIframe("videoFrame");
-        return match[7];
-      
-    } else {
-
-        hideIframe("videoFrame");
-        nohideP("error");
-    }
+  if (match && match[2].length === 11) {
+      return match[2];
+  } else {
+      return null;
+  }
 }
-function nohideP(pId) {
-    var p = document.getElementById(pId);
-    if (p) {
-       
-      p.style.display = "inline-block";
-    }
-  }
-  function hideP(pId) {
-    var p = document.getElementById(pId);
-    if (p) {
-       
-      p.style.display = "none";
-    }
-  }
-  function hideIframe(iframId) {
-    var fram = document.getElementById(iframId);
-    if (fram) {
-       
-      fram.style.display = "none";
-    }
-  }
-  function nohideIframe(iframId) {
-    var fram = document.getElementById(iframId);
-    if (fram) {
-       
-      fram.style.display = "inline-block";
-    }
-  }
+
+function showElement(element) {
+  element.style.display = 'inline-block';
+}
+
+function hideElement(element) {
+  element.style.display = 'none';
+}
+
 const mainMenu = document.querySelector('.mainMenu');
 const closeMenu = document.querySelector('.closeMenu');
 const openMenu = document.getElementById('user-icon');
